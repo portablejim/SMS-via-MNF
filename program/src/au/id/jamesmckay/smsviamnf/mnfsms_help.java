dcab76169e76817e593cd93ede6e723c93e73db3
James McKay <dcseee-github@yahoo.com.au>
2016-09-09 14:15:00 +1000

    Some source code might be nice

---
+++ program/src/au/id/jamesmckay/smsviamnf/mnfsms_help.java
@@ -0,0 +1,17 @@
+package au.id.jamesmckay.smsviamnf;
+
+import au.id.jamesmckay.smsviamnf.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class mnfsms_help extends Activity {
+	public static final String SETTINGS_NAME = "MNFSMS-settings";
+
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help);
+        }
+}
